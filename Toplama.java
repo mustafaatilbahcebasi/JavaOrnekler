0fc2734e5f75c7a51566c4decbdf7cb70839b1e9
Mustafa Karakaplan <karakaplanm@users.noreply.github.com>
2014-10-24 00:35:11 +0300

    Create Toplama.java

---
+++ Toplama.java
@@ -0,0 +1,10 @@
+public class Toplama {
+
+  public static void main(String[] args){
+  int toplam=0;
+  for (int i=1;i<=100;i++)
+     toplam=toplam+i;
+     System.out.print("Sonuç=");
+     System.out.print(toplam);
+  }
+}
