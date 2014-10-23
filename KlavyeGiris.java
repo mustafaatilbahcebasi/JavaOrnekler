cc59384bd3c4f8fa8b40ca8ceb9250656d46c9b2
Mustafa Karakaplan <karakaplanm@users.noreply.github.com>
2014-10-24 00:34:21 +0300

    Create KlavyeGiris.java

---
+++ KlavyeGiris.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class KlavyeGiris {
+  public static void main(String args[]){
+     Scanner giris=new Scanner(System.in);
+     System.out.print("Sayı Gir :");
+     double a=giris.nextDouble();
+     System.out.println(a);
+  }
+}
